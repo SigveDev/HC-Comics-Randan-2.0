58ad6c98e124c9cc34bb7ba76049dc67b1bfd609
2derp4you <iam2derp4you@gmail.com>
2025-02-18 22:27:52 +0100

    Fix for frontpage toggle

--- src/components/admin/edit/editTitleForm.tsx
+++ src/components/admin/edit/editTitleForm.tsx
@@ -146,7 +146,7 @@ const EditTitleForm = () => {
                   value=""
                   className="sr-only peer"
                   onClick={() => setFrontpage(!frontpage)}
-                  defaultChecked={frontpage}
+                  checked={frontpage}
                 />
                 <div className="w-11 h-6 peer-focus:outline-none rounded-full peer bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-[--primary]"></div>
               </label>
